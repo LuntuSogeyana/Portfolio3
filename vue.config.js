e9f1089335ccacb35d3b376b1045f9e3ec776630
unknown <sogeyanaluntu@gmail.com>
2023-05-22 12:40:21 +0200

    update firebase

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// vue.config.js
+module.exports = {
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  }
+};
