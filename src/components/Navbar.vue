016aa98d843056eb37c08d084069e495f626de57
unknown <sogeyanaluntu@gmail.com>
2023-10-04 11:12:33 +0200

    hamburger

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,11 @@
         <img src="https://i.postimg.cc/KvLk1q53/new-logo-removebg-preview-1-removebg-preview.png" alt="Logo">
       </a>
     </div>
-    <ul>
+    <input type="checkbox" id="menu-toggle" class="menu-toggle-input">
+    <label class="menu-button-container" for="menu-toggle">
+      <div class="menu-button"></div>
+    </label>
+    <ul class="menu">
       <li><a href="#">Home</a></li>
       <li><a href="#about">About</a></li>
       <li><a href="#projects">Projects</a></li>
@@ -31,7 +35,67 @@ nav {
   z-index: 1;
   height: 70px;
   border-bottom: 2px solid yellow;
-  overflow-x: hidden;
+  /* overflow-x: hidden; */
+}
+
+.menu > li {
+  margin: 0 1rem;
+}
+
+.menu-button-container {
+  display: none;
+  height: 100%;
+  width: 30px;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.menu-button {
+  background-color: yellow;
+}
+
+.menu-toggle-input {
+  display: none;
+}
+
+.menu-button,
+.menu-button::before,
+.menu-button::after {
+  display: block;
+  background-color: #fff;
+  position: absolute;
+  height: 4px;
+  width: 30px;
+  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  border-radius: 2px;
+}
+
+.menu-button::before {
+  content: '';
+  margin-top: -8px;
+  background-color: yellow;
+}
+
+.menu-button::after {
+  content: '';
+  margin-top: 8px;
+  background-color: yellow;
+}
+
+.menu-toggle-input:checked + .menu-button-container .menu-button::before {
+  margin-top: 0px;
+  transform: rotate(45deg);
+}
+
+.menu-toggle-input:checked + .menu-button-container .menu-button::after {
+  margin-top: 0px;
+  transform: rotate(-45deg);
+}
+
+.menu-toggle-input:checked + .menu-button-container .menu-button {
+  background-color:  rgb(62, 61, 61);
 }
 
 ul {
@@ -44,14 +108,21 @@ ul {
   font-size: 20px;
   flex-wrap: wrap;
   justify-content: flex-end; 
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+.menu-toggle-input:checked + .menu-button-container + ul {
+  max-height: 200px; 
 }
 
 li {
   margin-left: 1rem;
 }
 
-li :hover,li:focus {
-  opacity: 50%;
+li:hover, li:focus {
+  opacity: 0.5;
 }
 
 a {
@@ -70,14 +141,47 @@ a {
   height: auto; 
 }
 
+@media (max-width: 767.20px){
+  .menu-button-container{
+    display: flex;
+  }
+
+  .menu {
+    position: absolute;
+    top: 70px; 
+    left: 0;
+    flex-direction: column;
+    width: 100%;
+    background-color: rgb(62, 61, 61);
+    transition: max-height 400ms cubic-bezier(0.23, 1, 0.32, 1);;
+  }
+  .menu-toggle-input:checked + .menu-button-container + ul {
+    /* max-height: 200px; Adjust the max-height as needed for your content */
+    max-height: none;
+
+  }
+
+  .menu > li {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0.5em;
+    width: 100%;
+    color: white;
+    background-color: #222;
+    border-bottom: 1px solid #444;
+  }
+
+
+}
+
 @media (min-width: 768px) {
   ul {
     padding: 0 100px;
     flex-wrap: nowrap; 
     justify-content: flex-start; 
+    max-height: none;
+    overflow: visible;
   }
 }
 </style>
-
-
-  
